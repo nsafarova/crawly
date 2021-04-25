a9952b74fced19c382397ab42678816e1fd41477
nsafarova <58469217+nsafarova@users.noreply.github.com>
2021-04-25 19:06:56 +0400

    upd

--- src/main/java/WebCrawler.java
+++ src/main/java/WebCrawler.java
@@ -6,8 +6,6 @@ import org.postgresql.copy.CopyManager;
 import org.postgresql.core.BaseConnection;
 
 import java.io.*;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.sql.*;
 import java.util.ArrayList;
@@ -157,83 +155,6 @@ public class WebCrawler implements Runnable {
                 e.printStackTrace();
             }
     }
-
-    /*
-    public static boolean robotSafe(URL url)
-    {
-        String strHost = url.getHost();
-
-        String strRobot = "http://" + strHost + "/robots.txt";
-        URL urlRobot;
-        try { urlRobot = new URL(strRobot);
-        } catch (MalformedURLException e) {
-            // something weird is happening, so don't trust it
-            return false;
-        }
-
-        String strCommands;
-        try
-        {
-            InputStream urlRobotStream = urlRobot.openStream();
-            byte b[] = new byte[1000];
-            int numRead = urlRobotStream.read(b);
-            strCommands = new String(b, 0, numRead);
-            while (numRead != -1) {
-                numRead = urlRobotStream.read(b);
-                if (numRead != -1)
-                {
-                    String newCommands = new String(b, 0, numRead);
-                    strCommands += newCommands;
-                }
-            }
-            urlRobotStream.close();
-        }
-        catch (IOException e)
-        {
-            return true; // if there is no robots.txt file, it is OK to search
-        }
-
-        if (strCommands.contains(DISALLOW)) // if there are no "disallow" values, then they are not blocking anything.
-        {
-            String[] split = strCommands.split("\n");
-            ArrayList<RobotRule> robotRules = new ArrayList<>();
-            String mostRecentUserAgent = null;
-            for (int i = 0; i < split.length; i++)
-            {
-                String line = split[i].trim();
-                if (line.toLowerCase().startsWith("user-agent"))
-                {
-                    int start = line.indexOf(":") + 1;
-                    int end   = line.length();
-                    mostRecentUserAgent = line.substring(start, end).trim();
-                }
-                else if (line.startsWith(DISALLOW)) {
-                    if (mostRecentUserAgent != null) {
-                        RobotRule r = new RobotRule();
-                        r.userAgent = mostRecentUserAgent;
-                        int start = line.indexOf(":") + 1;
-                        int end   = line.length();
-                        r.rule = line.substring(start, end).trim();
-                        robotRules.add(r);
-                    }
-                }
-            }
-
-            for (RobotRule robotRule : robotRules)
-            {
-                String path = url.getPath();
-                if (robotRule.rule.length() == 0) return true; // allows everything if BLANK
-                if (robotRule.rule == "/") return false;       // allows nothing if /
-
-                if (robotRule.rule.length() <= path.length())
-                {
-                    String pathCompare = path.substring(0, robotRule.rule.length());
-                    if (pathCompare.equals(robotRule.rule)) return false;
-                }
-            }
-        }
-        return true;
-    } */
 
     public Thread getThread() {
         return thread;
